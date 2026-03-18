b09579dcbacae401ad05dbbf3fd43a915f5ae6d7
Benjosm <92320536+Benjosm@users.noreply.github.com>
2026-03-18 16:52:05 -0500

    feat(user): created separate fcm token table (PEG-59)

---
+++ drizzle/0003_groovy_vargas.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "fcm_tokens" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" text NOT NULL,
+	"token" text NOT NULL,
+	"platform" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "fcm_tokens_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "created_at" timestamp DEFAULT now();--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "updated_at" timestamp DEFAULT now();--> statement-breakpoint
+ALTER TABLE "fcm_tokens" ADD CONSTRAINT "fcm_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "fcm_token";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "fcm_platform";
